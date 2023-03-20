a071806377107ac063fb4f546750fe4c47b6254e
azfarkassim <azfarkassim@gmail.com>
2023-03-20 14:51:21 +0800

    Modified ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -41,6 +41,13 @@ resource "aws_security_group" "ec2_security_group" {
         description = "ssh access"
         from_port = 22
         to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
